f71b2529860f71b724eb050e226818e370a5e551
alexalok <aleks00799@gmail.com>
2021-11-30 14:26:04 +0300

    Update RNCryptor to .NET Standard 2.0

--- RNCryptor/Decryptor.cs
+++ RNCryptor/Decryptor.cs
@@ -1,36 +1,31 @@
-using System;
-using System.IO;
-using System.Text;
-using System.Security.Cryptography;
 using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace RNCryptor
 {
     public class Decryptor : Cryptor
     {
-        
         public byte[] Decrypt(byte[] encryptedBytes, string password)
         {
-            PayloadComponents components = this.unpackEncryptedData(encryptedBytes);
+            PayloadComponents components = unpackEncryptedData(encryptedBytes);
 
-            if (!this.hmacIsValid(components, password))
-            {
+            if (!hmacIsValid(components, password))
                 return null;
-            }
 
-            byte[] key = this.generateKey(components.salt, password);
+            byte[] key = generateKey(components.salt, password);
 
             byte[] plaintextBytes = new byte[0];
 
-            switch (this.aesMode)
+            switch (aesMode)
             {
                 case AesMode.CTR:
                     // Yes, we are "encrypting" here.  CTR uses the same code in both directions.
-                    plaintextBytes = this.encryptAesCtrLittleEndianNoPadding(components.ciphertext, key, components.iv);
+                    plaintextBytes = encryptAesCtrLittleEndianNoPadding(components.ciphertext, key, components.iv);
                     break;
 
                 case AesMode.CBC:
-                    plaintextBytes = this.decryptAesCbcPkcs7(components.ciphertext, key, components.iv);
+                    plaintextBytes = decryptAesCbcPkcs7(components.ciphertext, key, components.iv);
                     break;
             }
 
@@ -69,26 +64,26 @@ namespace RNCryptor
             components.schema = binaryBytes.GetRange(0, 1).ToArray();
             offset++;
 
-            this.configureSettings((Schema)binaryBytes[0]);
+            configureSettings((Schema)binaryBytes[0]);
 
             components.options = binaryBytes.GetRange(1, 1).ToArray();
             offset++;
 
-            components.salt = binaryBytes.GetRange(offset, Cryptor.saltLength).ToArray();
+            components.salt = binaryBytes.GetRange(offset, saltLength).ToArray();
             offset += components.salt.Length;
 
-            components.hmacSalt = binaryBytes.GetRange(offset, Cryptor.saltLength).ToArray();
+            components.hmacSalt = binaryBytes.GetRange(offset, saltLength).ToArray();
             offset += components.hmacSalt.Length;
 
-            components.iv = binaryBytes.GetRange(offset, Cryptor.ivLength).ToArray();
+            components.iv = binaryBytes.GetRange(offset, ivLength).ToArray();
             offset += components.iv.Length;
 
             components.headerLength = offset;
 
-            components.ciphertext = binaryBytes.GetRange(offset, binaryBytes.Count - Cryptor.hmac_length - components.headerLength).ToArray();
+            components.ciphertext = binaryBytes.GetRange(offset, binaryBytes.Count - hmac_length - components.headerLength).ToArray();
             offset += components.ciphertext.Length;
 
-            components.hmac = binaryBytes.GetRange(offset, Cryptor.hmac_length).ToArray();
+            components.hmac = binaryBytes.GetRange(offset, hmac_length).ToArray();
 
             return components;
 
@@ -96,19 +91,15 @@ namespace RNCryptor
 
         private bool hmacIsValid(PayloadComponents components, string password)
         {
-            byte[] generatedHmac = this.generateHmac(components, password);
+            byte[] generatedHmac = generateHmac(components, password);
 
             if (generatedHmac.Length != components.hmac.Length)
-            {
                 return false;
-            }
 
             for (int i = 0; i < components.hmac.Length; i++)
             {
                 if (generatedHmac[i] != components.hmac[i])
-                {
                     return false;
-                }
             }
             return true;
         }
